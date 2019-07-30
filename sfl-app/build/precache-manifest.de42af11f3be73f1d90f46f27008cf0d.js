15e7f0bf83c34d71abc712d4a2af77800aa57ac8
easyadin <tobiayokunnu@gmail.com>
2019-07-30 21:58:11 +0400

    build deployment

---
+++ sfl-app/build/precache-manifest.de42af11f3be73f1d90f46f27008cf0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09d6223658db664e0b88f98f4613601",
+    "url": "//github.com/lmschw/StressFreeLearning.git/index.html"
+  },
+  {
+    "revision": "8abe44eef94024713172",
+    "url": "//github.com/lmschw/StressFreeLearning.git/static/css/main.9d43e2fd.chunk.css"
+  },
+  {
+    "revision": "23369aaf0fc9738a11f0",
+    "url": "//github.com/lmschw/StressFreeLearning.git/static/js/2.1756eca6.chunk.js"
+  },
+  {
+    "revision": "8abe44eef94024713172",
+    "url": "//github.com/lmschw/StressFreeLearning.git/static/js/main.b404a8bd.chunk.js"
+  },
+  {
+    "revision": "df53b837c15fb69d9b05",
+    "url": "//github.com/lmschw/StressFreeLearning.git/static/js/runtime~main.9824ccb3.js"
+  }
+]);
